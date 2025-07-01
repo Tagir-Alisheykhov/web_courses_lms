553c03d15934d2d1f68e054576ee2fadbf3ecd85
Tagir-Alisheykhov <ahitahir@yandex.ru>
2025-07-01 15:02:06 +0300

    The project structure has been created

---
+++ lms/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+
+
+class Course(models.Model):
+    """Модель учебного курса"""
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название курса',
+        help_text='Введите название курса'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Описание курса',
+        help_text='Введите описание курса'
+    )
+    preview = models.ImageField(
+        upload_to='lms/previews/',
+        blank=True,
+        null=True,
+        verbose_name='Превью'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    """Модель урока"""
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название урока',
+        help_text='Введите название урока'
+    )
+    description = models.CharField(
+        blank=True,
+        null=True,
+        verbose_name='Описание урока',
+        help_text='Введите описание урока'
+    )
+    preview = models.ImageField(
+        upload_to='lms/previews/',
+        blank=True,
+        null=True,
+        verbose_name='Превью'
+    )
+    video = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name='Ссылка на видео',
+        help_text='Введите ссылку на видео'
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        verbose_name='Название курса',
+        help_text='Выберите курс',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
